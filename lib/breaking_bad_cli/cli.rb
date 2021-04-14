d0f0fbd8c1e05fe90c2705db79720ad91e0aee20
Isaiah Montoya <idmontoya@yahoo.com>
2021-04-13 21:08:50 -0800

    updated menu

--- lib/breaking_bad_cli/cli.rb
+++ lib/breaking_bad_cli/cli.rb
@@ -13,6 +13,21 @@ class CLI
     def greet(name)
         puts "Hello #{name}, which Breaking Bad character do you want to learn about?"
     end
+
+    def menu
+         selection = user_input
+        if selection == "b"
+            #print character
+        elsif selection == "exit"
+            #exit application
+        else
+            #invalid message and bring back to beginning to choose again
+        end
+    end
+
+    def goodbye
+        
+    end
 end   
     
     
